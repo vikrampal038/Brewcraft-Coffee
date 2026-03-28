344be9a817d1d5dd82659ad89d6cc9b1dc5c4060
Aayush Pal <ayushsurajpal123@gmail.com>
2026-03-28 15:14:44 +0530

    some changes in the backend

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { ClerkProvider } from "@clerk/clerk-react";
 import "./index.css";
